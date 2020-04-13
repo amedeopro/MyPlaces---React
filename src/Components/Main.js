7ba783d263498f520d169108d9ba82ed908fdcf3
Amedeo Pro <amedeopro@me.com>
2020-04-13 09:50:06 +0200

    fix MyPlacesList with state

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,114 +3,122 @@ import Header from './Header'
 import MyPlacesList from './MyPlacesList'
 import Footer from './Footer'
 
-const attractions = [
-    {
-        "id": 1,
-        "name": "Museo del cinema di Torino",
-        "type": "Museo",
-        "latitude": 45.0693174,
-        "longitude": 7.69304484,
-        "address": "Via Montebello, 20 10124 Torino",
-        "phone": "+390118138511",
-        "img": "http://localhost:5002/1.jpg"
-    },
-    {
-        "id": 2,
-        "name": "Basilica di Superga",
-        "type": "Costruzione storica",
-        "latitude": 45.070982,
-        "longitude": 7.685676,
-        "address": "Strada Basilica di Superga, 73, 10132 Torino",
-        "phone": "+390118997456",
-        "img": "http://localhost:5002/2.jpg"
-    },
-    {
-        "id": 3,
-        "name": "Mole Antonelliana",
-        "type": "Costruzione storica",
-        "latitude": 45.0690246,
-        "longitude": 7.6932346,
-        "address": "Via Montebello, 10124 Torino",
-        "phone": "+390118138511",
-        "img": "http://localhost:5002/3.jpg"
-    },
-    {
-        "id": 4,
-        "name": "Piazza Castello",
-        "type": "Piazza",
-        "latitude": 45.070982,
-        "longitude": 7.685676,
-        "address": "Piazza Castello - 10122 Torino",
-        "phone": "",
-        "img": "http://localhost:5002/4.jpg"
-    },
-    {
-        "id": 5,
-        "name": "Planetario di Torino",
-        "type": "Planetario",
-        "latitude": 45.039719,
-        "longitude": 7.761858,
-        "address": "Via Osservatorio, 30 - 10025 Pino Torinese TO",
-        "phone": "+390118118740",
-        "img": "http://localhost:5002/5.jpg"
-    },
-    {
-        "id": 6,
-        "name": "Palazzo Madama",
-        "type": "Costruzione storica",
-        "latitude": 45.070982,
-        "longitude": 7.685676,
-        "address": "Piazza Castello - 10122 Torino",
-        "phone": "",
-        "img": "http://localhost:5002/6.jpg"
-    },
-    {
-        "id": 7,
-        "name": "Piazza Vittorio Veneto",
-        "type": "Piazza",
-        "latitude": 45.064258,
-        "longitude": 7.696276,
-        "address": "Piazza Vittorio Veneto - 10123 Torino",
-        "phone": "",
-        "img": "http://localhost:5002/7.jpg"
-    },
-    {
-        "id": 8,
-        "name": "Museo Egizio",
-        "type": "Museo",
-        "latitude": 45.06843,
-        "longitude": 7.684304,
-        "address": "Via Accademia delle Scienze, 6 - 10123 Torino",
-        "phone": "+390115617776",
-        "img": "http://localhost:5002/8.jpg"
-    },
-    {
-        "id": 9,
-        "name": "Reggia di Venaria",
-        "type": "Costruzione Storica",
-        "latitude": 45.135972,
-        "longitude": 7.623365,
-        "address": "Piazza della Repubblica, 4 - 10078 Venaria Reale TO",
-        "phone": "+390114992333",
-        "img": "http://localhost:5002/9.jpg"
-    },
-    {
-        "id": 10,
-        "name": "Borgo Medievale",
-        "type": "Borgo",
-        "latitude": 45.049045,
-        "longitude": 7.685201,
-        "address": "Viale Virgilio, 107 - 10126 Torino",
-        "phone": "+390114431701",
-        "img": "http://localhost:5002/10.jpg"
-    }
-]
-
 class Main extends Component{
+  state = {
+    attractions : [
+        {
+            "id": 1,
+            "name": "Museo del cinema di Torino",
+            "type": "Museo",
+            "latitude": 45.0693174,
+            "longitude": 7.69304484,
+            "address": "Via Montebello, 20 10124 Torino",
+            "phone": "+390118138511",
+            "img": "http://localhost:5002/1.jpg"
+        },
+        {
+            "id": 2,
+            "name": "Basilica di Superga",
+            "type": "Costruzione storica",
+            "latitude": 45.070982,
+            "longitude": 7.685676,
+            "address": "Strada Basilica di Superga, 73, 10132 Torino",
+            "phone": "+390118997456",
+            "img": "http://localhost:5002/2.jpg"
+        },
+        {
+            "id": 3,
+            "name": "Mole Antonelliana",
+            "type": "Costruzione storica",
+            "latitude": 45.0690246,
+            "longitude": 7.6932346,
+            "address": "Via Montebello, 10124 Torino",
+            "phone": "+390118138511",
+            "img": "http://localhost:5002/3.jpg"
+        },
+        {
+            "id": 4,
+            "name": "Piazza Castello",
+            "type": "Piazza",
+            "latitude": 45.070982,
+            "longitude": 7.685676,
+            "address": "Piazza Castello - 10122 Torino",
+            "phone": "",
+            "img": "http://localhost:5002/4.jpg"
+        },
+        {
+            "id": 5,
+            "name": "Planetario di Torino",
+            "type": "Planetario",
+            "latitude": 45.039719,
+            "longitude": 7.761858,
+            "address": "Via Osservatorio, 30 - 10025 Pino Torinese TO",
+            "phone": "+390118118740",
+            "img": "http://localhost:5002/5.jpg"
+        },
+        {
+            "id": 6,
+            "name": "Palazzo Madama",
+            "type": "Costruzione storica",
+            "latitude": 45.070982,
+            "longitude": 7.685676,
+            "address": "Piazza Castello - 10122 Torino",
+            "phone": "",
+            "img": "http://localhost:5002/6.jpg"
+        },
+        {
+            "id": 7,
+            "name": "Piazza Vittorio Veneto",
+            "type": "Piazza",
+            "latitude": 45.064258,
+            "longitude": 7.696276,
+            "address": "Piazza Vittorio Veneto - 10123 Torino",
+            "phone": "",
+            "img": "http://localhost:5002/7.jpg"
+        },
+        {
+            "id": 8,
+            "name": "Museo Egizio",
+            "type": "Museo",
+            "latitude": 45.06843,
+            "longitude": 7.684304,
+            "address": "Via Accademia delle Scienze, 6 - 10123 Torino",
+            "phone": "+390115617776",
+            "img": "http://localhost:5002/8.jpg"
+        },
+        {
+            "id": 9,
+            "name": "Reggia di Venaria",
+            "type": "Costruzione Storica",
+            "latitude": 45.135972,
+            "longitude": 7.623365,
+            "address": "Piazza della Repubblica, 4 - 10078 Venaria Reale TO",
+            "phone": "+390114992333",
+            "img": "http://localhost:5002/9.jpg"
+        },
+        {
+            "id": 10,
+            "name": "Borgo Medievale",
+            "type": "Borgo",
+            "latitude": 45.049045,
+            "longitude": 7.685201,
+            "address": "Viale Virgilio, 107 - 10126 Torino",
+            "phone": "+390114431701",
+            "img": "http://localhost:5002/10.jpg"
+        }
+    ]
+  }
+
+  removeAttraction = (attraction) =>{
+    this.setState((state) =>({
+      attractions: state.attractions.filter((attr) => attr.id !== attraction.id)
+    }))
+  }
+
   render(){
     return <div>
       <Header titolo="MyPlaces" />
-      <MyPlacesList myplaces={attractions}/>
+      <MyPlacesList onRemoveAttraction={this.removeAttraction} myplaces={this.state.attractions}/>
       <Footer/>
     </div>
   }
--- src/Components/MyPlacesList.js
+++ src/Components/MyPlacesList.js
@@ -1,23 +1,41 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 
-class MyPlaceList extends Component{
-  render(){
-    return <ol className='place-list'>
-          {this.props.myplaces.map((myplace,index) => (
-            <li key={index} className='place-list-item'>
-              <div className='place-avatar' style={{
-                  background:`url(${myplace.img})`
-                }}>
-              </div>
-                <div className='place-details'>
-                  <p className='place-title'>{myplace.name}</p>
-                  <p>{myplace.type}</p>
+function MyPlaceList(props){
+      return <ol className='place-list'>
+            {props.myplaces.map((myplace,index) => (
+              <li key={index} className='place-list-item'>
+                <div className='place-avatar' style={{
+                    background:`url(${myplace.img})`
+                  }}>
                 </div>
-                <button className='place-remove'>Remove</button>
-            </li>))}
-        </ol>
-  }
+                  <div className='place-details'>
+                    <p className='place-title'>{myplace.name}</p>
+                    <p>{myplace.type}</p>
+                  </div>
+                  <button onClick={()=>props.onRemoveAttraction(myplace)} className='place-remove'>Remove</button>
+              </li>))}
+          </ol>
 }
+
+
+// class MyPlaceList extends Component{
+//   render(){
+//     return <ol className='place-list'>
+//           {this.props.myplaces.map((myplace,index) => (
+//             <li key={index} className='place-list-item'>
+//               <div className='place-avatar' style={{
+//                   background:`url(${myplace.img})`
+//                 }}>
+//               </div>
+//                 <div className='place-details'>
+//                   <p className='place-title'>{myplace.name}</p>
+//                   <p>{myplace.type}</p>
+//                 </div>
+//                 <button className='place-remove'>Remove</button>
+//             </li>))}
+//         </ol>
+//   }
+// }
 
 export default MyPlaceList
